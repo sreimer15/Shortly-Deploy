e7b3982ea254b213ffd8acafbb1de6f2805c9efa
Sean Reimer <sreimer15@gmail.com>
2016-01-02 17:00:02 -0600

    rebuild

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = require('./server.js');
 
- var port = 4568;
+var port = process.env.PORT || 4568;
+ // process.env.PORT = 4568
+// app.set('port', (process.env.PORT || 4568));
 
-app.listen(port);
+app.listen(port, function(){
+  console.log('Server now listening on port ' + port);
+});
 
-console.log('Server now listening on port ' + port);
+
+
